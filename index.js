35cfe36087487e4cbb46e5add1ad46f04a635215
tierney03 <tierney_03@mac.com>
2022-10-10 10:13:16 -0500

    Correcting structure and fleshing out index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-* {
-    backgroundcolor: lightgrey
-}
+//Packages required for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+const employee = require('./lib/employee.js')
+const manager = require('./lib/manager.js')
+const intern = require('./lib/intern.js')
+const engineer = require('./lib/engineer.js')
+const OUTPUT_DIR = path.resolve(__dirname, 'output');
+const outputPath = path.join(OUTPUT_DIR, 'teamprofiles.html')
+const generateTeamProfiles = require('')
+
+//Array for team
+
