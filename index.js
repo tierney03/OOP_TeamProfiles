b641b4f499314a923fa870e5c5df52fba18e1b7e
tierney03 <tierney_03@mac.com>
2022-10-10 11:16:06 -0500

    Creating the functions for app running in index.js

--- index.js
+++ index.js
@@ -8,7 +8,134 @@ const intern = require('./lib/intern.js')
 const engineer = require('./lib/engineer.js')
 const OUTPUT_DIR = path.resolve(__dirname, 'output');
 const outputPath = path.join(OUTPUT_DIR, 'teamprofiles.html')
-const generateTeamProfiles = require('')
+const generateTeamProfiles = require('./src/template.js');
+const Manager = require('./lib/manager.js');
+const Engineer = require('./lib/engineer.js');
+const Intern = require('./lib/intern.js');
 
 //Array for team
 
+function runApp() {
+    function createTeam() {
+        inquirer.createPromptModule([{
+            type: 'list',
+            message: 'What type of employee do you want to add to your team?',
+            name: 'addEmployee',
+            options: ['Manager', 'Engineer', 'Intern', 'No more team members.']
+        }]) .then(function(userInput) {
+            switch(userInput.addEmployee) {
+                case 'Manager':
+                    addManager();
+                    break;
+                    case 'Engineer':
+                    addEngineer();
+                    break;
+                    case 'Intern':
+                    addIntern();
+                    break;
+                    default:
+                    htmlBuilder()
+            }
+        })
+    }
+}
+
+// Adding team functions
+
+function addManager() {
+    inquirer.prompt ([
+    {
+        type: 'input',
+        name: 'managerName',
+        message: "What is the manager's name?"
+    },
+    {
+        type: 'input',
+        name: 'managerId',
+        message: "What is the manager's employee ID?"
+    },
+    {
+        type: 'input',
+        name: 'managerEmail',
+        message: "What is the manager's email?"
+    },
+    {
+        type: 'input',
+        name: 'managerOfficeNumber',
+        message: "What is the manager's office number?"
+    },
+
+    ]) .then(answers => {
+        const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
+        teamArray.push(manager);
+        createTeam();
+    });
+}
+
+function addEngineer() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'engineerName',
+            message: "What is the engineer's name?"
+        },
+        {
+            type: 'input',
+            name: 'engineerId',
+            message: "What is the engineer's employee ID?"
+        },
+        {
+            type: 'input',
+            name: 'engineerEmail',
+            message: "What is the engineer's email?"
+        },
+        {
+            type: 'input',
+            name: 'engineerGithub',
+            message: "What is the engineer's GitHub username?"
+        }
+    ]).then(answers => {
+        const engineer = new Engineer(answers.engineerName, answers.engineerID, answers.engineerEmail, answers.engineerGithub);
+        teamArray.push(engineer);
+        createTeam();
+    });
+
+    function addIntern() {
+        inquirer.prompt([
+        {
+            type: 'input',
+            name: 'internName',
+            message: "What is the intern's name?"
+        },
+        {
+            type: 'input',
+            name: 'internId',
+            message: "What is the intern's employee ID?"
+        },
+        {
+            type: 'input',
+            name: 'internEmail',
+            message: "What is the intern's email?"
+        },
+        {
+            type: 'input',
+            name: 'internSchool',
+            message: "What is the intern's school?"
+        }
+
+    ]).then(answers => {
+        const intern = new Intern(answers.internName, answers.interID, answers.internEmail, answers.internSchool);
+        teamArray.push(intern);
+        createTeam();
+    });
+    }
+}
+
+function htmlBuilder() {
+    console.log('Team Created!')
+    fs.writeFileSync(outputPath, generateTeam(teamArray), 'UTF-8')
+}
+
+createTeam();
+
+runApp()
